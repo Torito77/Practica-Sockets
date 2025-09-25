b4c2ed10cff06318dd992ab1678f349426cc9a9f
Torito <jorge.toro.mdz@gmail.com>
2025-09-24 20:18:25 -0600

    initial commit

---
+++ name_server.py
@@ -0,0 +1,28 @@
+import socket
+from server import Server
+from typing import Dict
+
+# TODO: Reduce code by creating parent Server class
+class NameServer(Server):
+    def __init__(self, host="localhost", port=1234):
+        self.server_addreses: Dict[str,str] = {}
+        super().__init__(host, port)
+
+    def server_start(self):
+        while True:
+            data, addr = self.sock.recvfrom(1024)
+            msg = data.decode().strip()
+            print(f"[{addr}]: {msg}")
+            parts = msg.split()
+            
+            # REGISTER name ip port
+            if len(parts) == 4 and parts[0] == "REGISTER":
+                _, name, ip, port = parts
+                self.server_addreses[name] = f"{ip}:{port}"
+                        
+            # LOOKUP name
+            elif len(parts) == 2 and parts[0] == "LOOKUP":
+                name = parts[1]
+                ans = self.server_addreses.get(name, "Not found")
+                self.sock.sendto(ans.encode(), addr)
+                        
---
+++ server.py
@@ -0,0 +1,16 @@
+import socket
+from abc import ABC, abstractmethod
+
+class Server(ABC):
+    def __init__(self, host="localhost", port=1234):
+        self.host = host
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind((host, port))
+        
+        print(f"Server running on {self.host}:{self.port}")
+        self.server_start()
+        
+    @abstractmethod
+    def server_start():
+        pass
